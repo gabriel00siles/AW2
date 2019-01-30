a2395aee4b84c152cd61eb604943525d3505a255
smx2 <smx2@ALUMNAT.LOCAL>
2019-01-30 20:05:35 +0100

    consulta de una base de datos

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+
+    <title>Futbol</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/carousel.css" rel="stylesheet">
+	
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand">Trabajo de AW / Gabriel Omar Siles</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+			  <li class="Mostrar">
+                  <a href="#Mostrar" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mostrar <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="mostrar1.php">Mostrar 1</a></li>
+                    <li><a href="mostrar2.php">Mostrar 2</a></li>
+                  </ul>
+                </li>
+                <li><a href="insertar.php">Insertar</a></li>
+                <li><a href="modificar.php">Modificar</a></li>
+                <li><a href="eliminar.php">Eliminar</a></li>
+                
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
+
+
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="img/fondo2.png" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1></h1>
+              <p></p>
+              
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="second-slide" src="img/fondo4.png" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1></h1>
+			  <p></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="third-slide" src="img/fondo3.png" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1></h1>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+	
+	<!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+   
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2019 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+	
+  </body>
+</html>
---
+++ mostrar1.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+
+    <title>Futbol</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/carousel.css" rel="stylesheet">
+	
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand">Tabla de equipos</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+			  <li><a href="index.php">Inicio</a></li>
+			  <li class="Mostrar">
+                  <a href="#Mostrar" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mostrar <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="mostrar1.php">Mostrar 1</a></li>
+                    <li><a href="mostrar2.php">Mostrar 2</a></li>
+                  </ul>
+                </li>
+                <li><a href="insertar.php">Insertar</a></li>
+                <li><a href="modificar.php">Modificar</a></li>
+                <li><a href="eliminar.php">Eliminar</a></li>
+                
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
+
+
+	
+	<!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+<br><br><br><br>
+
+		<?php
+			$servidor = "localhost";
+			$usuari = "root";
+			$password = "";
+			$db = "futbol";
+
+			// Create connection
+			$conn = new mysqli($servidor, $usuari, $password, $db);
+			
+			// Check connection
+			if ($conn->connect_error) {
+				die("Connexió KO: " . $conn->connect_error);
+			} 
+			else {
+
+				$sql = "SELECT codi, nom, localitat, web, escut FROM equips";
+				$result = $conn->query($sql);
+
+				if ($result->num_rows > 0) {
+					// output data of each row
+					echo "<div class=\"table-responsive\"> 
+					      <table class=\"table table-striped\">
+						  <thead>
+							<tr>
+							  <th>codi</th>
+							  <th>nom</th>
+							  <th>localitat</th>
+							  <th>web</th>
+							  <th>escut</th>
+							</tr>
+						  </thead>
+						  <tbody>";
+					while($rows = $result->fetch_assoc()) {
+						echo "<tr><td>" . $rows['codi'] . "</td><td>" . $rows['nom'] . "</td><td>". $rows['localitat']. "</td><td><a target=BLANK href=\"". $rows['web'] ."\">" . $rows['web'] . "</td><td><img src=\"". $rows['escut'] ."\">" . "</td></tr>";
+					}
+					echo "</tbody>
+						  </table>
+						  </div>";
+					
+				} else {
+					echo "0 registres";
+				}
+				$conn->close();
+			}
+		?>
+
+
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2019 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+	
+  </body>
+</html>
